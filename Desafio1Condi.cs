c7bc0fcecf8c8be7fedcce60cb3bc5feee140dff
JoJo Vanegas <46961682+boovanegas@users.noreply.github.com>
2019-01-31 15:55:03 -0500

    Add files via upload

---
+++ Desafio1Condi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1.Condi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar el Smmlv
+            Console.WriteLine("Ingresar el Smmlv");
+            int Smmlv = int.Parse(Console.ReadLine());
+
+
+
+
+            if (Smmlv < 2)
+            {
+                Console.WriteLine("Tarifa A");
+            }
+            else if ((2 <= Smmlv && Smmlv < 4))
+            {
+                Console.WriteLine("Tarifa B");
+            }
+            if (Smmlv >= 4)
+
+            {
+                Console.WriteLine("Tarifa C");
+            }
+
+        }
+    }
+}
---
+++ Desafio2Condi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_2.Condi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar el Smmlv
+            Console.WriteLine("Ingresar el Smmlv");
+            int Smmlv = int.Parse(Console.ReadLine());
+
+
+
+
+            if (Smmlv < 2)
+            {
+                Console.WriteLine("Tarifa A");
+                Console.WriteLine("Valor Cuota Moderadora 3,200");
+            }
+            else if ((2 <= Smmlv && Smmlv < 5))
+            {
+                Console.WriteLine("Tarifa B");
+                Console.WriteLine("Valor Cuota Moderadora 12,700");
+
+            }
+            if (Smmlv >= 5)
+
+            {
+                Console.WriteLine("Tarifa C");
+                Console.WriteLine("Valor Cuota Moderadora 35,500");
+            }
+
+        }
+    }
+}
