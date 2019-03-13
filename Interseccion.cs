fc455797258a4e22e0e72d120a149f1146b9f183
JoJo Vanegas <46961682+boovanegas@users.noreply.github.com>
2019-03-12 22:43:49 -0500

    Add files via upload

---
+++ Interseccion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Conjunto1 = { 0, 3, 6, 9, 12, 15, 18 };
+            int[] Conjunto2 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+            int[] union = new int[17];
+            int[] inter = new int[31];
+            int interr = 0;
+
+            Console.WriteLine("interseccion entre conjuntos");
+
+            for (int b = 0; b < Conjunto2.Length; b += 1)  
+            {
+                for (int c = 0; c < Conjunto1.Length; c += 1)
+                {
+                    if (Conjunto2[b] == Conjunto1[c])
+                    {
+                        inter[interr] = Conjunto2[b];
+                        Console.WriteLine(inter[interr]);
+                        interr += 1;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Union.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1hhh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Conjunto1 = { 0, 3, 6, 9, 12, 15, 18 };
+            int[] Conjunto2 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+            int[] union = new int[Conjunto1.Length + Conjunto2.Length];
+
+            for (int i = 0; i < Conjunto1.Length; i++)
+            {
+
+                union[i] = Conjunto1[i];
+            }
+            for (int i = Conjunto1.Length; i < union.Length; i++)
+            {
+
+                union[i] = Conjunto2[i - 7];
+            }
+            Console.WriteLine("Union:  ");
+            for (int i = 0; i < union.Length; i++)
+            {
+
+                Console.WriteLine("" + union[i]);
+
+            }
+        
+        }
+    }
+}
