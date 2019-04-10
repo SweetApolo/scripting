e45c56cf12b49e82c7b527d69ef8aaa9b938b31a
JoJo Vanegas <46961682+boovanegas@users.noreply.github.com>
2019-04-09 20:00:18 -0500

    Add files via upload

---
+++ Ej2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+
+    Renderer mcolor;
+    float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mcolor = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        Color r = (Color.red);
+        Color a = (Color.blue);
+        Color m = (Color.red + Color.blue);
+        mcolor.material.color = r;
+        if (t <= 3)
+        {
+            mcolor.material.color = r;
+        }
+
+        if (t > 3 || t == 6)
+        {
+            mcolor.material.color = a;
+        }
+
+        if (t >= 6)
+        {
+            mcolor.material.color = m;
+        }
+
+    }
+}
