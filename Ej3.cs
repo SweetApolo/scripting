81fdc602ed2c1997fc5332718d6b4b5a0678c3a0
JoJo Vanegas <46961682+boovanegas@users.noreply.github.com>
2019-04-09 20:46:08 -0500

    Add files via upload

---
+++ Ej3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej3 : MonoBehaviour
+{
+    Light mluz;
+    Light mcolor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mluz = GetComponent<Light>();
+        mcolor = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Color r = (Color.red);
+        Color a = (Color.blue);
+        Color w = (Color.white);
+        if (mluz.intensity >= 0.25f || mluz.intensity == 0.5f)
+        {
+            mcolor.color = a;
+        }
+
+        if (mluz.intensity >= 0.5f || mluz.intensity == 0.75f)
+        {
+            mcolor.color = r;
+        }
+
+        if (mluz.intensity >= 0.75f )
+        {
+            mcolor.color = w;
+        }
+    }
+}
